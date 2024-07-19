1869a0c901eb0bcaed4667bdeb2f2a9da3e87200
Omerkugel12 <omerkugel12@gmail.com>
2024-07-19 17:50:17 +0300

    test1

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { getUser } from "../controllers/user.controller";
 
 const userRouter = Router();
-userRouter.get("/", getToken);
-userRouter.post("/", registerUser);
+// userRouter.get("/", getToken);
+// userRouter.post("/", registerUser);
 userRouter.get("/:id", getUser);
 
 export default userRouter;
