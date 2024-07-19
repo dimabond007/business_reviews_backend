9618099328ac74b5fe00355e9e97760cbb0caca2
Dmitriy <dimistma@gmail.com>
2024-07-19 11:31:41 +0300

    asdf

--- src/models/Business.ts
+++ src/models/Business.ts
@@ -1,11 +1,16 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
+
 interface IBusiness extends Document {
   name: string;
   description: string;
+  user: Types.ObjectId;
 }
+
 const businessSchema = new Schema<IBusiness>({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
+
 const Business = model<IBusiness>("Business", businessSchema);
 export default Business;
