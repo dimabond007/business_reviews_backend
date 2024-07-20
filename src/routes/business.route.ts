605f81540ae1934337cc54eefc73f25e4ee45767
Dmitriy <dimistma@gmail.com>
2024-07-20 15:13:04 +0300

    add like

--- src/routes/business.route.ts
+++ src/routes/business.route.ts
@@ -11,10 +11,10 @@ import { verifyToken } from "../middleware/auth.middleware";
 
 const businessRouter = Router();
 businessRouter.get("/", getAllBusiness);
+businessRouter.patch("/review/:id/", updateReview);
 businessRouter.get("/review/:id/like", verifyToken, toggleLike);
 businessRouter.get("/:id/reviews/", getReviewsByBusinessId);
 businessRouter.post("/:id/reviews/", createReview);
-businessRouter.patch("/:id/reviews", updateReview);
 businessRouter.delete("/:id/reviews/:reviewId", deleteReview);
 
 export default businessRouter;
