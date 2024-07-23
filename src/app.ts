777e31ef6b3f800c07fa84caa1782284e08e593f
Dmitriy <dimistma@gmail.com>
2024-07-23 15:28:25 +0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,18 @@ const app: Application = express();
 
 async function main() {
   await connectDB();
-
+  app.use(express.static("public"));
   app.use(express.json());
   app.use(cors({}));
 
+  const path = require("path");
+
   app.use("/api/user", userRoutes);
   app.use("/api/business", businessRouter);
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
 }
 main();
 
