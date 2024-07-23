2bf4ef1ab5c14e27ca97e223b360d441ff4b77ea
dimabond007 <dimistma@gmail.com>
2024-07-23 21:59:35 +0300

    asd

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,26 @@ import userRoutes from "./routes/user.route";
 import businessRouter from "./routes/business.route";
 import connectDB from "./config/db";
 import cors from "cors";
+import path from 'path';
 
 const app: Application = express();
 
 async function main() {
   await connectDB();
-  app.use(express.static("public"));
+  
   app.use(express.json());
   app.use(cors({}));
 
-  const path = require("path");
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.use("/api/user", userRoutes);
   app.use("/api/business", businessRouter);
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 }
+
 main();
 
 export default app;
-//f5r
